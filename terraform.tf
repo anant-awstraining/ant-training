3b1a393a5f1f7c7cb2b0db24af7ac5df65f4323f
anant-awstraining <145030951+anant-awstraining@users.noreply.github.com>
2023-09-16 01:16:24 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,6 @@
 provider "aws" {
 region     = "ap-south-1"
-  access_key = "AKIA5JVJFC2I336Z6UR6"
-  secret_key = "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"
+ 
 }
 
 resource "aws_instance" "ec2" {
